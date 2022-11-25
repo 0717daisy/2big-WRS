fb775ebaa399c966f9adfd784d877cba82c10920
0717daisy <dclimato@gmail.com>
2022-11-25 20:20:42 +0800

    UI onlyyyy

--- 2big-WRS/Controllers/HomeController.cs
+++ 2big-WRS/Controllers/HomeController.cs
@@ -29,15 +29,63 @@ namespace _2big_WRS.Controllers
         {
             return View();
         }
+        public ActionResult POS()
+        {
+            return View();
+        }
+        public ActionResult WaterOrders()
+        {
+            return View();
+        }
+        public ActionResult Reservations()
+        {
+            return View();
+        }
+        public ActionResult Station()
+        {
+            return View();
+        }
+        public ActionResult WaterRefill()
+        {
+            return View();
+        }
+        public ActionResult Gallons()
+        {
+            return View();
+        }
+        public ActionResult Employees()
+        {
+            return View();
+        }
+        public ActionResult WaterProduct()
+        {
+            return View();
+        }
         public ActionResult AdminProfile()
         {
             return View();
         }
-        public ActionResult Deliveries()
+        public ActionResult ListGraph()
         {
             return View();
         }
         public ActionResult Reports()
+        {
+            return View();
+        }
+        public ActionResult DeliveryRecords()
+        {
+            return View();
+        }
+        public ActionResult DeliveryDetails()
+        {
+            return View();
+        }
+        public ActionResult Reviews()
+        {
+            return View();
+        }
+        public ActionResult Rewards()
         {
             return View();
         }
